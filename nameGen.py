94f1a3a21b3076bb078ea70828c3ef5d576cf237
Justin <Justin.Jackson134@gmail.com>
2018-03-03 15:57:07 -0800

    added name files

--- nameGen.py
+++ nameGen.py
@@ -1,1 +1,39 @@
-def
+import random
+
+human_male_first_names = []
+human_female_first_names = []
+humanSurnames = []
+
+
+# read names from name lists
+
+
+
+
+# human name generators
+# human names
+
+
+def get_first_name_human_male():
+    return random.choice(open("file").readLines())
+
+
+def get_full_name_human_male():
+    return "First Name"
+
+
+def get_first_name_human_female():
+    return "First Name"
+
+
+def get_full_name_human_female():
+    return "First Name"
+
+
+def get_surname_name_human():
+    return "First Name"
+
+
+# human cities
+def get_civilization_name():
+    return "Generic Village"
--- tileTest.py
+++ tileTest.py
@@ -1,11 +1,15 @@
 import tile
 import drawing
 import WorldGen
+import nameGen
 
 tiles = tile.Tile((0, 0), "plains", 0, "wild")
 print(tiles.owner)
 
 world_map = tile.generate_tilemap(20)
+
+while True:
+    print(nameGen.get_first_name_human_male())
 
 while True:
     for row in world_map:
