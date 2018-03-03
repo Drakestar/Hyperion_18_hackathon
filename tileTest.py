a01b3273837bab57b9d73c34ba5b3a88cfdb36ae
Justin <Justin.Jackson134@gmail.com>
2018-03-03 12:40:37 -0800

    implemented generation of default tilemap in tile.py
    added printout of tilemap coordinates in tileTest.py

--- tileTest.py
+++ tileTest.py
@@ -1,4 +1,12 @@
 import tile
 
-tiles = tile.Tile("plains", 0, "wild")
+tiles = tile.Tile((0,0), "plains", 0, "wild")
 print(tiles.owner)
+
+world_map = tile.generate_tilemap(5)
+
+for row in world_map:
+    temp_str = ""
+    for tile in row:
+        temp_str = temp_str + "\t(" + str(tile.coordinate[0]) + "," + str(tile.coordinate[1]) + ")"
+    print(temp_str + "\n")
