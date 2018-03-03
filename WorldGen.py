08f7ad4a5ce7f6c5704807fe9097fca9a977d645
Jacob Sadoian <jmsadoian@users.noreply.github.com>
2018-03-03 13:24:02 -0800

    First implementation of basic terrain generation

---
+++ WorldGen.py
@@ -0,0 +1,42 @@
+import tile
+import random
+import numpy as np
+
+
+def generate_terrain(world_map):
+    terrain_list = ['mountain', 'hill', 'forest', 'grassland']
+    exotic_probability = .05
+    size = len(world_map) - 1
+    for row in world_map:
+        for tile in row:
+            if tile.coordinate[0] == 0 or tile.coordinate[1] == 0 or tile.coordinate[0] == size or tile.coordinate[
+                1] == size:
+                tile.terrain = 'deep ocean'
+            else:
+                neighbors_list = get_neighbors(world_map, tile)
+                if neighbors_list[0].terrain == neighbors_list[1].terrain:
+                    neighbor_probability = .66
+                    new_terrain = 1 - neighbor_probability
+                    prob_list = [new_terrain, neighbor_probability]
+                    choices = [1, 2]
+                    selection = np.random.choice(choices, p=prob_list)
+
+                else:
+                    neighbor_probability_1 = .33
+                    neighbor_probability_2 = .33
+                    new_terrain = 1 - neighbor_probability_1 - neighbor_probability_2
+                    prob_list = [new_terrain, neighbor_probability_1, neighbor_probability_2]
+                    choices = [1, 2, 3, 4]
+                    selection = np.random.choice(choices, p=prob_list)
+
+                if selection == 1:
+                    tile.terrain = terrain_list[np.random_intergers() % 4]
+                if selection == 2:
+                    tile.terrain = neighbors_list[0].terrain
+                if selection == 3:
+                    tile.terrain = neighbors_list[1].terrain
+
+
+def get_neighbors(world_map, tile):
+    neighbors = [world_map[tile[0] - 2][tile[1] - 1], world_map[tile[0] - 1][tile[1] - 2]]
+    return neighbors
