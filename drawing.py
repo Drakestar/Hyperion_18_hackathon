63091d125194cac3accecd671f4e8bc29f06d236
josh <joshua.carver.prog@gmail.com>
2018-03-03 19:59:58 -0800

    It works i swear

--- drawing.py
+++ drawing.py
@@ -1,5 +1,7 @@
 import pygame
+from pygame.locals import *
 import constants
+
 
 
 # Given a terrain name it will return a color to be used
@@ -21,6 +23,7 @@ def terrain_color(terrain_name):
 # Draws the map given the tileset, window details, and window itself
 def draw_map(tiles, display):
     windowwidth, windowheight = pygame.display.get_surface().get_size()
+    windowwidth -= 300
     # Set Square corners to 0
     x1 = y1 = x2 = y2 = 0
     # Go through each row of the tileset
@@ -76,3 +79,38 @@ class Block(pygame.sprite.Sprite):
     # Put this back in game loop if use
     # all_sprites_list.update()
     # all_sprites_list.draw(display)
+
+
+def get_indices(screenwidth, screenheight, tile_amount):
+    # Get the mouse position then do calculation to get index of clicked tile
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    mouse_x = int(mouse_x / (screenwidth / tile_amount))
+    mouse_y = int(mouse_y / (screenheight / tile_amount))
+    # If the mouse click is passed the furthest tile
+    if mouse_x > tile_amount:
+        mouse_x = tile_amount - 1
+    if mouse_y > tile_amount:
+        mouse_y = tile_amount - 1
+    return mouse_x, mouse_y
+
+
+def tile_info(tile):
+    myfont = pygame.font.SysFont("monospace", 25)
+    label_list = []
+    # render text
+    label1 = myfont.render("Owner: " + tile.owner, 1, (0, 0, 0))
+    label2 = myfont.render("Terrain: " + tile.terrain, 1, (0, 0, 0))
+    label3 = myfont.render("Population: " + str(tile.population), 1, (0, 0, 0))
+    label4 = myfont.render("Contains: " + tile.contains, 1, (0, 0, 0))
+    label_list.append(label1)
+    label_list.append(label2)
+    label_list.append(label3)
+    label_list.append(label4)
+    return label_list
+
+
+def draw_text(display, label_list):
+    text_y = 10
+    for x in label_list:
+        display.blit(x, (710, text_y))
+        text_y += 30
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Import statements
 import pygame
+import sys
+import constants
 from pygame.locals import *
 import tile
 import WorldGen
@@ -10,29 +12,63 @@ def main():
     # Start pygame and create the display at a start of 700x700
     pygame.init()
     pygame.display.set_caption("RPG Storyteller")
-    display = pygame.display.set_mode((700, 700), 0, 32)
+    display = pygame.display.set_mode((1000, 700), 0, 32)
     # Used to manage how fast the screen updates
     clock = pygame.time.Clock()
     # Create map
-    tile_map = tile.generate_tilemap(10)
+    tile_map = tile.generate_tilemap(constants.WORLDSIZE)
     WorldGen.generate_terrain(tile_map)
     # Constants
     width, height = pygame.display.get_surface().get_size()
-    # Main "game" loop
-    while True:
+    width -= 300
+    label_list = []
+    myfont = pygame.font.SysFont("monospace", 25)
+    label1 = myfont.render("YES", 1, (0, 0, 0))
+    label2 = myfont.render("NO", 1, (0, 0, 0))
+    label_list.append(label1)
+    label_list.append(label2)
+    loop_var = True
+    while loop_var:
         # Draw map
         drawing.draw_map(tile_map, display)
+        pygame.draw.rect(display, constants.GRAY, constants.SQUAREOFTRUTH)
+        display.blit(label1, (835, 175))
+        display.blit(label2, (835, 525))
         # Look through all events
         for event in pygame.event.get():
             # Quit
             if event.type == QUIT:
                 pygame.quit()
+                sys.exit()
+            # User clicks a square it gives information
             if event.type == MOUSEBUTTONDOWN:
-                # print event.button
                 mouse_x, mouse_y = pygame.mouse.get_pos()
-                mouse_x = int(mouse_x / (width / len(tile_map)))
-                mouse_y = int(mouse_y / (height / len(tile_map)))
-                print(tile_map[mouse_y][mouse_x].terrain)
+                if mouse_x > 700:
+                    if mouse_y > 350:
+                        WorldGen.generate_terrain(tile_map)
+                    else:
+                        loop_var = False
+        # Draw the text when tile clicked
+        #drawing.draw_text(display, label_list)
+        pygame.display.update()
+    # Main "game" loop
+    while True:
+        # Draw map
+        drawing.draw_map(tile_map, display)
+        pygame.draw.rect(display, constants.GRAY, constants.SQUAREOFTRUTH)
+        # Look through all events
+        for event in pygame.event.get():
+            # Quit
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            # User clicks a square it gives information
+            if event.type == MOUSEBUTTONDOWN:
+                tile_x, tile_y = drawing.get_indices(width, height, len(tile_map))
+                print(tile_x, tile_y, len(tile_map))
+                label_list = drawing.tile_info(tile_map[tile_y][tile_x])
+        # Draw the text when tile clicked
+        drawing.draw_text(display, label_list)
         pygame.display.update()
 
 
