3c2f5ad8de31e114e224472443d22f34e1e5c459
josh <joshua.carver.prog@gmail.com>
2018-03-03 16:58:20 -0800

    Josh pretends to do work

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 # Import statements
 import pygame
 from pygame.locals import *
-import constants
 import tile
+import WorldGen
 import drawing
 
 
 def main():
     # Start pygame and create the display at a start of 700x700
     pygame.init()
+    pygame.display.set_caption("RPG Storyteller")
     display = pygame.display.set_mode((700, 700), 0, 32)
-    display.fill(constants.WHITE)
+    # Used to manage how fast the screen updates
+    clock = pygame.time.Clock()
     # Create map
-    tile_set = tile.generate_tilemap(10)
-    #
-    #
-    drawing.draw_map(tile_set, display)
+    tile_map = tile.generate_tilemap(50)
+    WorldGen.generate_terrain(tile_map)
+    # Constants
+    # Main "game" loop
     while True:
+        # Draw map
+        drawing.draw_map(tile_map, display)
+        # Look through all events
         for event in pygame.event.get():
+            # Quit
             if event.type == QUIT:
                 pygame.quit()
         pygame.display.update()
